4ff772594778b12576c5ddc787d676475044af40
dzobamain <knopkayt26@gmail.com>
2025-04-21 16:34:36 +0200

    Added .bat for windows compilation.
    build.sh
      Added error checking during compilation

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set OUT=autoclicker.exe
+
+for /R %%f in (*.cpp) do (
+    set SRC=!SRC! "%%f"
+)
+
+g++ -std=c++11 %SRC% -o %OUT% -lncurses
+
+if %ERRORLEVEL%==0 (
+    echo Compilation successful. Output: %OUT%
+) else (
+    echo Compilation failed.
+)
+
+pause
--- build.sh
+++ build.sh
@@ -4,4 +4,10 @@ OUT="autoclicker"
 
 SRC=$(find . -name "*.cpp")
 
-g++ -std=c++11 $SRC -o $OUT -framework ApplicationServices -lncurses
+g++ -std=c++11 $SRC -o $OUT -lncurses
+
+if [ $? -eq 0 ]; then
+    echo "Compilation successful. Output: $OUT"
+else
+    echo "Compilation failed."
+fi
