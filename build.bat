f4e7e682c68a3e62d138245c31a10dbf9717c71e
dzobamain <knopkayt26@gmail.com>
2025-09-14 20:35:39 +0200

    Updeted build.*

--- build.bat
+++ build.bat
@@ -1,23 +1,19 @@
 @echo off
-set OUT=autoclicker
+set OUT=autoclicker.exe
 set BUILD_DIR=build
 
-if not exist "%BUILD_DIR%" (
-    mkdir "%BUILD_DIR%"
-)
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
 
-REM
-set SRC=src\main.cpp src\choose.cpp src\Autocliker.cpp src\AutoclikerMenu.cpp
+set SRC=src\main.cpp src\choose.cpp src\Autocliker.cpp src\AutoclikerMenu.cpp src\settings.cpp src\lang.cpp
+set INCLUDE=-Iinclude
 
-REM 
-set INCLUDE_DIRS=-Iinclude
+echo Compiling...
 
-REM
-g++ -std=c++17 %SRC% %INCLUDE_DIRS% -o %BUILD_DIR%\%OUT% -lncurses
+g++ -std=c++17 %SRC% %INCLUDE% -o %BUILD_DIR%\%OUT% -lncurses
 
-IF %ERRORLEVEL% EQU 0 (
+if %ERRORLEVEL% equ 0 (
     echo Compilation successful. Output: %BUILD_DIR%\%OUT%
-) ELSE (
+) else (
     echo Compilation failed.
 )
 pause
--- build.sh
+++ build.sh
@@ -14,14 +14,16 @@ SRC=(
     src/lang.cpp
 )
 
-INCLUDE_DIRS=(
-    -Iinclude
-)
+INCLUDE_DIRS=(-Iinclude)
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
 
 g++ -std=c++17 "${SRC[@]}" "${INCLUDE_DIRS[@]}" -o "$BUILD_DIR/$OUT" -lncurses -framework ApplicationServices
 
 if [ $? -eq 0 ]; then
-    echo "Compilation successful. Output: $BUILD_DIR/$OUT"
+    echo -e "${GREEN}Compilation successful. Output: $BUILD_DIR/$OUT${NC}"
 else
-    echo "Compilation failed."
+    echo -e "${RED}Compilation failed.${NC}"
 fi
